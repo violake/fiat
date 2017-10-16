57569ccee53deaf47954a9e82797eded5dfee331
Roger Fang <roger.fang@acx.io>
2017-10-16 12:00:58 +1100

    [project transfer] to rails ui project. FiatCLI importCSV params not ready yet

--- fiatCLI.rb
+++ fiatCLI.rb
@@ -1,6 +1,5 @@
 require_relative './config/environment'
 require 'thor'
-require_relative './config/fiat_config'
 require_relative './app/services/payment_import'
 
 
@@ -12,16 +11,20 @@ class FiatCLI < Thor
   #
   def initialize(args = [], local_options = {}, config = {})
     super(args, local_options, config)
-    @fiat_config = FiatConfig.new
-    puts @fiat_config[:log_level]
-    
   end
 
-  desc "importCSV csv_file_name timezone", "check payments csv file data and import into database then sending to ACX via MQ"
-  def importCSV(file, timezone)
+  desc "importCSV csv_file_name --timezone '+08:00' --source_type 'westpac' --currency 'aud'", "check payments csv file data and import into database then sending to ACX via MQ"
+  option :timezone
+  option :source_type
+  option :currency
+  def importCSV(file)
     begin
-      raise "timezone format error: '#{timezone}'. eg: [+08:00] " if  !timezone || ! (/^[+\-](0\d|1[0-2]):([0-5]\d)$/.match(timezone) )
-      puts Fiat::PaymentImport.new.importPaymentsFile(file, timezone)
+      #raise "--timezone format error: '#{options[:timezone]}'. eg: [+08:00] " if  ! (/^[+\-](0\d|1[0-2]):([0-5]\d)$/.match(options[:timezone]) )
+      #raise "--invalid bank account: '#{options[:bank_account]}' " if !options[:bank_account] && ! Fiat::FUND_TYPE.include?(options[:bank_account])
+      params = options.inject({}) {|hash, (k,v)| hash.merge!({k.to_sym=>v})}
+      params[:timezone] ||= "+08:00"
+      params[:bank_account] ||= "1204938740218302"
+      puts Fiat::PaymentImport.new.importPaymentsFile(file, params)
     rescue Exception=>e
       puts e.message
     end  
