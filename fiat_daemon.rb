5c466eb46490fe63095576d39622607f31cca0fa
Roger Fang <roger.fang@acx.io>
2017-12-01 14:18:01 +1100

    [FiatMailer] refactoring Fiatd daemon with graceful shutdown, sending email to admin if cannot connect to RabbitMQ

--- fiat_daemon.rb
+++ fiat_daemon.rb
@@ -4,36 +4,19 @@ require_relative './config/fiat_config.rb'
 require_relative './service/fiatd'
 require_relative './service/fiat-mailer'
 
-fiatd = Fiatd.new(FiatConfig.new)
-
-logger = fiatd.logger
-
-terminate = proc do
-  puts "Terminating threads .."
-  fiatd.shutdown
-  puts "Stop"
-end
-
-clean_terminate = proc do
-  if fiatd.busy
-    fiatd.busy = !fiatd.busy
-  else
-    puts "Not busy"
-    terminate.call
-  end
-end
-
-Signal.trap("TERM",  &clean_terminate)
-Signal.trap("INT", &terminate)
-
-logger.info("Fiat daemon start !")
 begin
+  fiatd = Fiatd.new(FiatConfig.new)
+  logger = fiatd.logger
+  logger.info("Fiat daemon start !")
   fiatd.start
 rescue Bunny::TCPConnectionFailed => e
-  msg = "Connect to RabbitMQ failed. Please check MQ server address."
-  logger.error(msg)
+  msg = "Connect to RabbitMQ failed.\nPlease check MQ server address."
+  puts msg
   opts={subject: "ALERT: Fiat MQ",
         body: "Fiat Error: #{msg}"}
   FiatMailer.send_email(FiatConfig.new[:fiat_email][:admin_email], opts)
-  exit(1)
+rescue SignalException =>e
+  puts "Terminating process .."
+  fiatd.shutdown
+  puts "Stopped."
 end
