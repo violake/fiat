a91848e4a1d101a31e2f65e88ed944f035df6ade
Roger Fang <roger.fang@acx.io>
2018-01-11 17:19:24 +1100

    [rspec bug] fix transfer_out_import require bug and return hash key bug

--- spec/service/transfer_out_import_spec.rb
+++ spec/service/transfer_out_import_spec.rb
@@ -1,21 +1,23 @@
 require 'rails_helper'
+require './app/services/transaction_import.rb'
+require './app/services/transfer_out_import'
 
-RSpec.describe TransferOutImport do
+RSpec.describe Fiat::TransferOutImport do
 
   before(:all) { @to = Fiat::TransferOutImport.new }
 
   describe 'import westpac statements' do
     
     it 'return correct result when import normal westpac statement csv' do
-      params = {timezone: "+08:00", bank_account: "033152468666", source_type: "westpac"}
+      params = {timezone: "+08:00", bank_account: "033152468666", source_type: "westpac", currency: "aud"}
       file = File.new(Rails.root.join("spec/factories/transfer_outs_normal.csv"))
       response = @to.importTransferOutFile(file, params)
 
-      expect(response['result']['imported']).to eq(6)
-      expect(response['result']['ignored']).to eq(0)
-      expect(response['result']['error']).to eq(1)
-      expect(response['result']['sent']).to eq(5)
-      expect(response['result']['filtered']).to eq(5)
+      expect(response[:imported]).to eq(6)
+      expect(response[:ignored]).to eq(0)
+      expect(response[:error]).to eq(1)
+      expect(response[:sent]).to eq(5)
+      expect(response[:filtered]).to eq(5)
     end
 
   end
